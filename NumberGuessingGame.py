87f67e1da31872c8b7d98c06be74746356c670ab
Arjun Kurimindla <134851253+arjunkurimindla@users.noreply.github.com>
2026-02-02 15:00:02 +0530

    Add files via upload

---
+++ NumberGuessingGame.py
@@ -0,0 +1,25 @@
+def playagain():
+    import random
+    num=random.randint(1,100)
+    return num
+count=1
+guess=0
+nu=playagain()
+while nu!=guess:
+    try:
+        guess = int(input("guess the number :"))
+        if guess<nu:
+            print(f"Guessed number {guess} is low!! enter greater number")
+            count+=1
+        elif guess>nu:
+            print(f"Guessed number {guess} is high!! enter lesser number")
+            count+=1
+        else:
+            print(f"Guessed number {guess} matches with number {nu} in {count} attempts")
+            play=input("want to play again {y/n}").lower()
+            nu = playagain()
+            if play!="y":
+                break
+
+    except ValueError:
+        print("enter valid input")
